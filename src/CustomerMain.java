5ac803151553f3aaf42c015f8ad79ec889532d46
Alireza Fakari <arshiafakary@gmail.com>
2026-03-16 22:43:59 +0100

    S7 - constructor overloading example

---
+++ src/CustomerMain.java
@@ -0,0 +1,19 @@
+public class CustomerMain {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Alireza",12345.5,"alireza@gmail.com");
+
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getName());
+        System.out.println(customer.getEmailAddress());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getEmailAddress());
+
+        Customer ThirdCustomer = new Customer("John","john@gmail.com");
+        System.out.println(ThirdCustomer.getCreditLimit());
+        System.out.println(ThirdCustomer.getName());
+        System.out.println(ThirdCustomer.getEmailAddress());
+    }
+}
